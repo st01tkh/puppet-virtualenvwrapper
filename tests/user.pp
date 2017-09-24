472294e2aec88a6a72efaf52c26605f495d707f9
st01tkh <st01tkh@gmail.com>
2017-09-24 01:26:57 +0000

    Added proj_dir_rel_path, add_projects_home

--- tests/user.pp
+++ tests/user.pp
@@ -2,9 +2,10 @@ $user = 'user01'
 virtualenvwrapper::user {"$user venvs": 
   user => $user,
   use_home_var => true,  
+  envs_dir_rel_path => 'venvs',
+  proj_dir_rel_path => 'projs',
 }
 
-  #envs_dir_rel_path => 'venvs',
 #
 # vim: tabstop=2 shiftwidth=2 expandtab
 # 
